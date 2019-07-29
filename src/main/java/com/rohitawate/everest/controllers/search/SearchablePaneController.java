0f492f9151c8ba75021492ba472a40bc900b05ee
Vladimir Stojanovic <st.vladas@hotmail.com>
2019-07-29 11:40:51 +0200

    Everest

--- src/main/java/com/rohitawate/everest/controllers/search/SearchablePaneController.java
+++ src/main/java/com/rohitawate/everest/controllers/search/SearchablePaneController.java
@@ -39,158 +39,165 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 
 public abstract class SearchablePaneController<T> implements Initializable {
-    @FXML
-    private StackPane searchPromptLayer, searchLayer, searchFailedLayer;
-    @FXML
-    private JFXButton clearSearchFieldButton;
-    @FXML
-    private TextField searchTextField;
-    @FXML
-    private VBox searchTab, searchBox, searchPane;
+	@FXML
+	private StackPane searchPromptLayer, searchLayer, searchFailedLayer;
+	@FXML
+	private JFXButton clearSearchFieldButton;
+	@FXML
+	private TextField searchTextField;
+	@FXML
+	private VBox searchTab, searchBox, searchPane;
 
-    private List<Searchable<T>> searchableItems;
+	private List<Searchable<T>> searchableItems;
 
-    protected static class SearchEntry<T> {
-        private final Parent fxmlItem;
-        private final Searchable<T> searchable;
+	protected static class SearchEntry<T> {
+		private final Parent fxmlItem;
+		private final Searchable<T> searchable;
 
-        public SearchEntry(Parent fxmlItem, Searchable<T> searchable) {
-            super();
-            this.fxmlItem = fxmlItem;
-            this.searchable = searchable;
-        }
+		public SearchEntry(Parent fxmlItem, Searchable<T> searchable) {
+			super();
+			this.fxmlItem = fxmlItem;
+			this.searchable = searchable;
+		}
 
-        public Parent getFxmlItem() {
-            return fxmlItem;
-        }
+		public Parent getFxmlItem() {
+			return fxmlItem;
+		}
 
-        public Searchable<T> getSearchable() {
-            return searchable;
-        }
-    }
+		public Searchable<T> getSearchable() {
+			return searchable;
+		}
+	}
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        searchableItems = new ArrayList<>();
-        searchLayer.visibleProperty().bind(searchTextField.textProperty().isNotEmpty());
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		searchableItems = new ArrayList<>();
+		searchLayer.visibleProperty().bind(searchTextField.textProperty().isNotEmpty());
 
-        searchTextField.textProperty().addListener(((observable, oldValue, newValue) -> {
-            searchBox.getChildren().remove(0, searchBox.getChildren().size());
-            searchFailedLayer.setVisible(false);
-            List<Searchable<T>> searchResults = getSearchResults(searchTextField.getText());
+		searchTextField.textProperty().addListener(((observable, oldValue, newValue) -> {
+			searchBox.getChildren().remove(0, searchBox.getChildren().size());
+			searchFailedLayer.setVisible(false);
+			List<Searchable<T>> searchResults = getSearchResults(searchTextField.getText());
 
-            searchResults.sort((controller1, controller2) -> {
-                int relativity1 = controller1.getRelativityIndex(searchTextField.getText());
-                int relativity2 = controller2.getRelativityIndex(searchTextField.getText());
-                return relativity2 - relativity1;
-            });
+			searchResults.sort((controller1, controller2) -> {
+				int relativity1 = controller1.getRelativityIndex(searchTextField.getText());
+				int relativity2 = controller2.getRelativityIndex(searchTextField.getText());
+				return relativity2 - relativity1;
+			});
 
-            if (searchResults.size() != 0) {
-                for (Searchable<T> controller : searchResults) {
-                    addSearchItem(controller.getState());
-                }
-            } else {
-                searchFailedLayer.setVisible(true);
-            }
-        }));
+			if (searchResults.size() != 0) {
+				for (Searchable<T> controller : searchResults) {
+					addSearchItem(controller.getState());
+				}
+			} else {
+				searchFailedLayer.setVisible(true);
+			}
+		}));
 
-        clearSearchFieldButton.setOnAction(e -> searchTextField.clear());
+		clearSearchFieldButton.setOnAction(e -> searchTextField.clear());
 
-        Platform.runLater(this::loadInitialItemsAsync);
-    }
+		Platform.runLater(this::loadInitialItemsAsync);
+	}
 
-    private void loadInitialItemsAsync() {
-        Task<List<T>> entryLoader = new Task<List<T>>() {
-            @Override
-            protected List<T> call() {
-                return loadInitialEntries();
-            }
-        };
+	private void loadInitialItemsAsync() {
+		Task<List<T>> entryLoader = new Task<List<T>>() {
+			@Override
+			protected List<T> call() {
+				return loadInitialEntries();
+			}
+		};
 
-        entryLoader.setOnSucceeded(e -> {
-            try {
-                List<T> entries = entryLoader.get();
-                if (entries.size() == 0) {
-                    searchPromptLayer.setVisible(true);
-                    return;
-                }
+		entryLoader.setOnSucceeded(e -> {
+			try {
+				List<T> entries = entryLoader.get();
+				if (entries.size() == 0) {
+					searchPromptLayer.setVisible(true);
+					return;
+				}
 
-                for (T state : entries)
-                    addHistoryItem(state);
-            } catch (InterruptedException | ExecutionException E) {
-                LoggingService.logSevere("Task thread interrupted while populating HistoryTab.", E,
-                        LocalDateTime.now());
-            }
-        });
+				for (T state : entries)
+					addHistoryItem(state);
+			} catch (InterruptedException | ExecutionException E) {
+				LoggingService.logSevere("Task thread interrupted while populating HistoryTab.", E,
+						LocalDateTime.now());
+			}
+		});
 
-        entryLoader.setOnFailed(e -> LoggingService.logWarning("Failed to load history.",
-                (Exception) entryLoader.getException(), LocalDateTime.now()));
+		entryLoader.setOnFailed(e -> LoggingService.logWarning("Failed to load history.",
+				(Exception) entryLoader.getException(), LocalDateTime.now()));
 
-        MoreExecutors.directExecutor().execute(entryLoader);
-    }
+		MoreExecutors.directExecutor().execute(entryLoader);
+	}
 
-    private void addSearchItem(T state) {
-        appendToList(state, searchBox, false);
-    }
+	private void addSearchItem(T state) {
+		appendToList(state, searchBox, false);
+	}
 
-    protected abstract List<T> loadInitialEntries();
+	protected abstract List<T> loadInitialEntries();
 
-    public void focusSearchField() {
-        searchTextField.requestFocus();
-    }
+	public void focusSearchField() {
+		searchTextField.requestFocus();
+	}
 
-    private Searchable<T> appendToList(T state, VBox layer, boolean appendToStart) {
-        searchPromptLayer.setVisible(false);
-        try {
-            SearchEntry<T> searchEntry = createEntryFromState(state);
+	private Searchable<T> appendToList(T state, VBox layer, boolean appendToStart) {
+		searchPromptLayer.setVisible(false);
+		try {
+			SearchEntry<T> searchEntry = createEntryFromState(state);
 
-            if (appendToStart)
-                layer.getChildren().add(0, searchEntry.getFxmlItem());
-            else
-                layer.getChildren().add(searchEntry.getFxmlItem());
+			if (appendToStart)
+				layer.getChildren().add(0, searchEntry.getFxmlItem());
+			else
+				layer.getChildren().add(searchEntry.getFxmlItem());
 
-            return searchEntry.getSearchable();
-        } catch (IOException e) {
-            LoggingService.logSevere("Could not append HistoryItem to list.", e, LocalDateTime.now());
-        }
+			return searchEntry.getSearchable();
+		} catch (IOException e) {
+			LoggingService.logSevere("Could not append HistoryItem to list.", e, LocalDateTime.now());
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    protected abstract SearchEntry<T> createEntryFromState(T state) throws IOException;
+	protected abstract SearchEntry<T> createEntryFromState(T state) throws IOException;
 
-    public void addHistoryItem(T state) {
-        Searchable<T> controller = appendToList(state, searchTab, true);
-        searchableItems.add(controller);
-    }
+	public void addHistoryItem(T state) {
+		Searchable<T> controller = appendToList(state, searchTab, true);
+		searchableItems.add(controller);
+	}
 
-    private List<Searchable<T>> getSearchResults(String searchString) {
-        List<Searchable<T>> filteredList = new ArrayList<>();
+	public void removeAllHistoryItems(T state) {
+		Searchable<T> controller = appendToList(state, searchTab, true);
+		for (Searchable<T> searchable : searchableItems) {
+			searchableItems.remove(controller);
+		}
+	}
 
-        for (Searchable<T> controller : searchableItems) {
+	private List<Searchable<T>> getSearchResults(String searchString) {
+		List<Searchable<T>> filteredList = new ArrayList<>();
 
-            int relativityIndex = controller.getRelativityIndex(searchString);
+		for (Searchable<T> controller : searchableItems) {
 
-            // Split the string into words and get total relativity index as sum of
-            // individual indices.
-            String words[] = searchString.split("\\s");
-            for (String word : words)
-                relativityIndex += controller.getRelativityIndex(word);
+			int relativityIndex = controller.getRelativityIndex(searchString);
 
-            if (relativityIndex != 0)
-                filteredList.add(controller);
-        }
+			// Split the string into words and get total relativity index as sum of
+			// individual indices.
+			String words[] = searchString.split("\\s");
+			for (String word : words)
+				relativityIndex += controller.getRelativityIndex(word);
 
-        return filteredList;
-    }
+			if (relativityIndex != 0)
+				filteredList.add(controller);
+		}
 
-    public void toggleVisibilityIn(SplitPane splitPane) {
-        if (searchPane.isVisible()) {
-            searchPane = (VBox) splitPane.getItems().remove(0);
-        } else {
-            splitPane.getItems().add(0, searchPane);
-        }
+		return filteredList;
+	}
 
-        searchPane.setVisible(!searchPane.isVisible());
-    }
+	public void toggleVisibilityIn(SplitPane splitPane) {
+		if (searchPane.isVisible()) {
+			searchPane = (VBox) splitPane.getItems().remove(0);
+		} else {
+			splitPane.getItems().add(0, searchPane);
+		}
+
+		searchPane.setVisible(!searchPane.isVisible());
+	}
 }
